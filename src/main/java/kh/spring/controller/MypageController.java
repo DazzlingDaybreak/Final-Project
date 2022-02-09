9275d27f8ea2bded87ed0939be50cf96445950e5
DazzlingDaybreak <95536303+DazzlingDaybreak@users.noreply.github.com>
2022-02-09 11:54:16 +0900

    Revert "인증 사진 불러오기 수정"

--- src/main/java/kh/spring/controller/MypageController.java
+++ src/main/java/kh/spring/controller/MypageController.java
@@ -182,7 +182,6 @@ public class MypageController {
 			int certiCheck = mService.certiCheck(certiDate);
 			if(certiCheck==1) {
 				model.addAttribute("certiCheck",certiCheck);
-				System.out.println("중복검사 : " + certiCheck);
 			}
 		}
 		CertiDTO info = new CertiDTO();
